8a8594970ed4b46ecbfe23c388ac4148eeb757c5
ocean <18565619803@163.com>
2018-10-17 10:01:06 +0800

    项目构建完成

--- src/main/java/ApplicationTest.java
+++ src/main/java/ApplicationTest.java
@@ -1,16 +1,13 @@
-import com.controller.TestController;
+
 import com.service.TestService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
 
 public class ApplicationTest {
     public  static void main(String[] args){
         ApplicationContext ctx = new ClassPathXmlApplicationContext("springMVC-main.xml");
         TestService tc = (TestService) ctx.getBean("testService");
         tc.toTest2();
-        System.out.println(23333);
     }
 }
